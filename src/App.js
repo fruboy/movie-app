a24a33ad13c2c68c37b1e8b88dae9348950c781b
fruboy <frazhaider15@gmail.com>
2021-01-17 22:04:38 +0500

    Favourites stored on local storage

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavourites from './components/AddToFavourites'
+import RemoveFavourites from './components/RemoveFavourites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [favourites, setFavourites] = useState([]);
-  const [keyword, setkeyword] = useState("");
+  const [keyword, setkeyword] = useState("2020");
 
   const getMovieRequest = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
@@ -21,10 +22,31 @@ const App = () => {
   const addFavouriteMovie = (movie) => {
     const newFavouriteList = [...favourites, movie];
     setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
   };
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+    saveToLocalStorage(newFavouriteList);
+    setFavourites(newFavouriteList);
+  };
+  useEffect(() => {
+    const movieFavourites = JSON.parse(
+      localStorage.getItem('react-movie-app-favourites')
+    );
+    if (movieFavourites != null)
+      setFavourites(movieFavourites);
+    else
+      setFavourites([]);
+  }, []);
   useEffect(() => {
     getMovieRequest(keyword);
   }, [keyword]);
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+  };
+
   return (
     <div className='container-fluid movie-app'>
       <div className='row d-flex align-items-center mt-4 mb-4'>
@@ -38,7 +60,10 @@ const App = () => {
         <MovieListHeading heading='Favourites' />
       </div>
       <div className='row'>
-        <MovieList movies={favourites} favouriteComponent={AddFavourites} />
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouriteMovie}
+          favouriteComponent={RemoveFavourites} />
       </div>
     </div>
   );
